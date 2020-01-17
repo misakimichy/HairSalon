9bce617ec5f639a4aea4e41506dff8f1228dcea8
Misaki Koonce <misaki.koonce@gmail.com>
2020-01-17 10:32:08 -0800

    Add Home.

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HairSalon.Models
+{
+    public class Stylist
+    {
+        public int StylistId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- HairSalon/Startup.cs
+++ HairSalon/Startup.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using HairSalons.Models;
+using HairSalon.Models;
 
-namespace HairSalons
+namespace HairSalon
 { 
   public class Startup
   {
@@ -24,7 +24,7 @@ namespace HairSalons
       services.AddMvc();
 
       services.AddEntityFrameworkMySql()
-        .AddDbContext<HairSalonsContext>(options => options
+        .AddDbContext<HairSalonContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
     }
     public void Configure(IApplicationBuilder app)
