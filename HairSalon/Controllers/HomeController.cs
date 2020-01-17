959ff0a5871b9280cc671ea4e2aaf3bbace8fa5f
Misaki Koonce <misaki.koonce@gmail.com>
2020-01-17 11:46:27 -0800

    Fix the error. Successfully redirect to Stylists/Index and add data to database.

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -1,12 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;  
-using HairSalon.Models;
 
 namespace HairSalon.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("/")]
+        [HttpGet("/")]
         public ActionResult Index()
         {
             return View();
